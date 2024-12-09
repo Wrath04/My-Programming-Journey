7f3f9a2cef226c036da906e6eacc789375a08656
Gluttony04 <ajosunda696@gmail.com>
2024-12-09 08:09:37 +0700

    Add files via upload

---
+++ LKPD_12.ino
@@ -0,0 +1,20 @@
+//XI TAV 3
+ #include"DHT.h"
+ #define DHTPIN 32
+ #define DHTTYPE DHT11
+ DHT dht(DHTPIN, DHTTYPE);
+ void setup(){
+  Serial.begin(9600);
+  dht.begin();}
+void loop(){
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  if (isnan(humidity)||isnan(temperature)){
+    Serial.println("Gagal membaca sensor DHT11");
+  }else{
+    Serial.print("Kelembaban: ");
+    Serial.print(humidity);
+    Serial.print("% Suhu: ");
+    Serial.print(temperature);
+    Serial.println("°C");}
+   delay(2000);}
